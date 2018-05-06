8a6d00cd4feae6d2d7ba65d2cbddcbc763fd0a0d
Michael J. Bondra <mjbondra@gmail.com>
2018-05-06 11:30:54 -0700

    simplify koa app used by integration tests

--- test/integration/helpers/koa-app.js
+++ test/integration/helpers/koa-app.js
@@ -3,18 +3,12 @@
 const Koa = require('koa');
 const koaRatelimit = require('koa-ratelimit');
 
-module.exports = ({ db, duration, errorMessage, max }) => {
+module.exports = ({ db, duration, max }) => {
   const app = new Koa();
-  let count = 0;
 
-  app.use(async (ctx, next) => {
-    await next();
-    if (ctx.body === errorMessage) ctx.body = { message: errorMessage };
-  });
-  app.use(koaRatelimit({ db, duration, errorMessage, max }));
+  app.use(koaRatelimit({ db, duration, max }));
   app.use(ctx => {
-    count++;
-    ctx.body = { count };
+    ctx.body = 'ok';
   });
 
   return app;
